952654d0d4441158a7455e8d58c78156056fbf3d
DulcheE <34923261+DulcheE@users.noreply.github.com>
2020-11-18 16:33:34 +0100

    Add SubTopic Base + Topic Full

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,8 +3,10 @@ import userRouter from './user.js' // after the import from express
 import universeRouter from './universe.js'
 import characterRouter from './character.js'
 import inventoryRouter from './inventory.js'
+import subTopicRouter from './subTopic.js'
 import templateCategoryRouter from './templateCategory.js'
 import templateStatRouter from './templateStat.js'
+import topicRouter from './topic.js'
 const apiRouter = Router()
 
 const apiRoute = '/api/v1/'
@@ -16,8 +18,10 @@ apiRouter.use('/users', userRouter) // before the export default
 apiRouter.use('/universes', universeRouter)
 apiRouter.use('/characters', characterRouter)
 apiRouter.use('/inventories', inventoryRouter)
+apiRouter.use('/sub-topics', subTopicRouter)
 apiRouter.use('/template-categories', templateCategoryRouter)
 apiRouter.use('/template-stats', templateStatRouter)
+apiRouter.use('/topics', topicRouter)
 
 apiRouter.get('/', (req, res) => {
   res.json({
@@ -25,8 +29,10 @@ apiRouter.get('/', (req, res) => {
       articles: `${baseAPI(req)}articles`,
       characters: `${baseAPI(req)}characters`,
       inventories: `${baseAPI(req)}inventories`,
+      subTopics: `${baseAPI(req)}sub-topics`,
       'template-categories': `${baseAPI(req)}template-categories`,
       'template-stats': `${baseAPI(req)}template-stats`,
+      topics: `${baseAPI(req)}topics`,
       universes: `${baseAPI(req)}universes`,
       users: `${baseAPI(req)}users`
     }
