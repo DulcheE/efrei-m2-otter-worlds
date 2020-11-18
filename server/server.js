59c58130072946ca2111453bb7558ce8f6527e08
DulcheE <34923261+DulcheE@users.noreply.github.com>
2020-11-18 19:13:59 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ import express from 'express'
 import session from 'express-session'
 import logger from 'morgan'
 import config from './server.config.js'
-import mariadbStore from './mariadb-store.js'
+// import mariadbStore from './mariadb-store.js'
 import { apiRoute, apiRouter } from './routes/routes'
 
 const app = express()
-mariadbStore.init(config.MARIADB)
+// mariadbStore.init(config.MARIADB)
 
 app.use(logger('dev'))
 app.use(session({
